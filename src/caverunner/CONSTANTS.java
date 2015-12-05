03e5a565dea803e2a0752ce197302462dc40551f
wcb <wcb@wcb-asus.attlocal.net>
2015-12-05 13:28:41 -0800

    Original version

---
+++ src/caverunner/CONSTANTS.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caverunner;
+
+/**
+ *
+ * @author wcb
+ */
+
+public class CONSTANTS {
+   public static final int WINDOW_WIDTH = 1232;
+   //public static final int WINDOW_WIDTH = 1222;
+   public static final int WINDOW_HEIGHT = 704;
+   public static final int BLOCK_WIDTH = 44;
+   public static final int BLOCK_HEIGHT = 44;
+   public static final int RUNNING_FRAME_DISTANCE = 8;
+   public static final int CLIMBING_FRAME_DISTANCE = 8;
+   public static final int FALLING_FRAME_DISTANCE = 8;
+   public static final int LEAPING_FRAME_DISTANCE = 8;
+   public static final int DEFAULT_FRAME_RATE_DIVIDER = 5;  // 5 gives ~ 12 f/s (60/5)
+   public static final String NO_KEY_PRESSED = "no key pressed";
+   public static final String RIGHT_ARROW = "RIGHT";
+   public static final String LEFT_ARROW = "LEFT";
+   public static final String UP_ARROW = "UP";
+   public static final String DOWN_ARROW = "DOWN";
+   public static final String CLEAR = "CLEAR";
+   public static final String HOME = "HOME";
+   public static final String PAGE_UP = "PAGE_UP";
+   }
